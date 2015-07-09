c285ccd64ffeb9f8bbe848dd77446b395bf8d9ac
Luke Savage <luke.savage92@gmail.com>
2015-07-08 21:29:23 -0500

    Implemented .hasMinorDiagonalConflictAt()

--- src/Board.js
+++ src/Board.js
@@ -173,7 +173,27 @@
     //
     // test if a specific minor diagonal on this board contains a conflict
     hasMinorDiagonalConflictAt: function(minorDiagonalColumnIndexAtFirstRow) {
-      return false; // fixme
+      var count = 0;
+      var board = this.rows();
+
+      var diagCheck = function(index, row){
+        if( index === 0 ){
+          return;
+        }
+
+        if(board[row][index] === 1){
+          count++;
+        }
+
+        diagCheck(index - 1, row + 1);
+      };
+
+      diagCheck(majorDiagonalColumnIndexAtFirstRow, 0);
+
+      if( count > 1 ){
+        return true;
+      }
+      return false;
     },
 
     // test if any minor diagonals on this board contain conflicts
